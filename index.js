f00c1c289d98ff8d30f8d42c76276614035831bb
beatriz <beatrizffeo@gmail.com>
2024-03-18 18:06:27 -0600

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution here
+function findMatching(drivers, searchString) {
+    const lowercaseSearch = searchString.toLowerCase();
+      const matchingDrivers = drivers.filter(driverName => {
+      return driverName.toLowerCase() === lowercaseSearch;
+    });
+  
+    return matchingDrivers;
+  }
+  function fuzzyMatch(drivers, query) {
+    return drivers.filter(driver => {
+        return driver.startsWith(query);
+    })
+  }
+  function matchName(drivers, name) {
+    const matchingDrivers = [];
+  
+    for (const driver of drivers) {
+      if (driver.name === name) {
+        matchingDrivers.push(driver);
+      }
+    }
+  
+    return matchingDrivers;
+  }
